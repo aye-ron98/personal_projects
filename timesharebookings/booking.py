f27ab676c0572e233c95e873148f304588f617f3
aye-ron98 <aron.zhang013@gmail.com>
2022-12-22 12:25:10 -0800

    decompose bookings into functions

--- timesharebookings/booking.py
+++ timesharebookings/booking.py
@@ -2,35 +2,101 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
-import time
 
 
-# def open_booking_site(site):
-#     """
-#     Open the embarc website and return a working driver link
-#
-#     :param site: valid URL to the embarc website
-#     :precondition site: website URL as a string
-#     :postcondition: a working driver link program will run on
-#     :return: site drive link
-#     """
+def login(se_driver):
+    """
+    login to embarc website
+
+    :param se_driver: a webdriver for selenium framework to use
+    :precondition se_driver: webdriver must be accessible by computer
+    :postcondition: will login to the embarc website
+    """
+
+    username = se_driver.find_element(By.XPATH, '//*[@id="Username"]')
+    username.send_keys('erwinzhang')
+
+    password = se_driver.find_element(By.XPATH, '//*[@id="Password"]')
+    password.send_keys('Safety2021')
+
+    logon = se_driver.find_element(By.XPATH, '//*[@id="page-mobile"]/div[3]/div[2]/div/form/div[3]/div[4]/input')
+    logon.click()
+
+
+def page_navigation(se_driver):
+    """
+    Navigate webpage
+
+    :param se_driver: a webdriver for selenium framework to use
+    :precondition se_driver: webdriver must be accessible by computer
+    :postcondition: will navigate to desired pages on website
+    """
+
+    navigate_to_bookings = se_driver.find_element(By.XPATH, '//*[@id="dashboard-content"]/div[2]/div/div[2]/a')
+    navigate_to_bookings.click()
+
+
+def check_for_bookings(se_driver, location, arrival, departure, nights, occupancy):
+    """
+    Check hotel site for available bookings, returns list of available bookings if
+    found else notifies user none are available.
+
+    :param se_driver: a webdriver for selenium framework to use
+    :param location: a valid embrac location
+    :param arrival: an arrival date greater than or equal to today's date
+    :param departure: the departure date greater than the arrival date
+    :param nights: number of nights user wishes to stay
+    :param occupancy: number of people staying in booking
+    :precondition se_driver: webdriver must be accessible by computer
+    :precondition: arrival and departure must be string in DD-Month-YYYY format
+    :precondition: nights and occupancy must be a number formatted as string type
+    :postcondition: will check the current openings and attempt to match them against user input
+    """
+
+    destination = se_driver.find_element(By.XPATH, '//*[@id="DestinationSearchTerm"]')
+    destination.send_keys(location)
+
+    arrival_date = se_driver.find_element(By.XPATH, '//*[@id="Booking_CheckInFromDate"]')
+    arrival_date.clear()
+    arrival_date.send_keys(arrival)
+
+    departure_date = se_driver.find_element(By.XPATH, '//*[@id="Booking_CheckInToDate"]')
+    departure_date.clear()
+    departure_date.send_keys(departure)
+
+    se_driver.execute_script("document.querySelector('#Booking_NightCount')."
+                             "setAttribute('value', '{}')".format(nights))
+    se_driver.execute_script("document.querySelector('#Booking_Occupancy')."
+                             "setAttribute('value', '{}')".format(occupancy))
+
+    se_driver.implicitly_wait(5)
+
+    se_driver.find_element(By.XPATH, '//*[@id="submit"]').submit()
+
+
+def print_avaliability(se_driver):
+    """
+    Check for room availability, if there is a match return a list of all available bookings
+
+    :param se_driver: valid selenium driver
+    :precondition se_driver: webdriver must be accessible by computer
+    :postcondition: if there is room availability will generate a list of available bookings
+    :return: list of tuples containing available bookings
+    """
+    avalability = se_driver.find_elements(By.CLASS_NAME, 'td-Checkin-Checkout')
+    avaliable_rooms = []
+    for checkin, checkout in zip(*[iter(avalability)] * 2):
+        avaliable_rooms.append(('checking: {0}, checkout: {1}'.format(checkin.text, checkout.text)))
+
+    return avaliable_rooms
 
 
 def main(url):
     driver = webdriver.Chrome()
     driver.get(url)
 
-    username = driver.find_element(By.XPATH, '//*[@id="Username"]')
-    username.send_keys('erwinzhang')
-
-    password = driver.find_element(By.XPATH, '//*[@id="Password"]')
-    password.send_keys('Safety2021')
-
-    login = driver.find_element(By.XPATH, '//*[@id="page-mobile"]/div[3]/div[2]/div/form/div[3]/div[4]/input')
-    login.click()
-
-    navigate_to_bookings = driver.find_element(By.XPATH, '//*[@id="dashboard-content"]/div[2]/div/div[2]/a')
-    navigate_to_bookings.click()
+    login(driver)
+    page_navigation(driver)
 
     try:
         WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located(
@@ -40,29 +106,8 @@ def main(url):
     else:
         driver.find_element(By.XPATH, '/html/body/div[11]/div[1]/button').click()
 
-    destination = driver.find_element(By.XPATH, '//*[@id="DestinationSearchTerm"]')
-    destination.send_keys('Embarc® Whistler')
-
-    arrival_date = driver.find_element(By.XPATH, '//*[@id="Booking_CheckInFromDate"]')
-    arrival_date.clear()
-    arrival_date.send_keys('22-Dec-2022')
-
-    departure_date = driver.find_element(By.XPATH, '//*[@id="Booking_CheckInToDate"]')
-    departure_date.clear()
-    departure_date.send_keys('31-Dec-2022')
-
-    driver.execute_script("document.querySelector('#Booking_NightCount').setAttribute('value', '1')")
-    driver.execute_script("document.querySelector('#Booking_Occupancy').setAttribute('value', '3')")
-
-    time.sleep(0.5)
-
-    driver.find_element(By.XPATH, '//*[@id="submit"]').submit()
-
-    avalability = driver.find_elements(By.CLASS_NAME, 'td-Checkin-Checkout')
-
-    for checkin, checkout in zip(*[iter(avalability)] * 2):
-        print('checking:', checkin.text, '|', 'checkout:', checkout.text)
-
+    check_for_bookings(driver, 'Embarc® Whistler', '01-May-2023', '08-May-2023', '2', '3')
+    print(print_avaliability(driver))
 
 
 if __name__ == '__main__':
